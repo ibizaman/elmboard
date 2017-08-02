94d097826983e696d3481b1a2480ef2d8c2c95d8
Pierre Penninckx <ibizapeanut@gmail.com>
2017-08-02 08:39:54 -0700

    send server shutdown message to frontend websockets

--- backend-py3/server.py
+++ backend-py3/server.py
@@ -1,10 +1,15 @@
-from aiohttp import web, WSMsgType
+from aiohttp import web, WSMsgType, WSCloseCode
 
 
 
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
+
+    if 'websockets' not in request.app:
+        request.app['websockets'] = set([ws])
+    else:
+        request.app['websockets'].add(ws)
 
     async for msg in ws:
         if msg.type == WSMsgType.TEXT:
@@ -21,9 +26,19 @@ async def websocket_handler(request):
     return ws
 
 
+async def stop_websockets(app):
+    if 'websockets' not in app:
+        return
+    for ws in app['websockets']:
+        await ws.close(code=WSCloseCode.GOING_AWAY, message='server shutdown')
+
+
 def main():
     app = web.Application()
     app.router.add_get('/socket', websocket_handler)
+
+    app.on_shutdown.append(stop_websockets)
+
     web.run_app(app)
 
 
